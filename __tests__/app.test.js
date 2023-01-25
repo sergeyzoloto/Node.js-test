1ec596464d5d6c794e9f08536bafac2d343e1cdb
Sergey Zolotko <sergiozoloto@gmail.com>
2023-01-25 15:40:35 +0100

    tests set up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,35 +1,91 @@
 import app from '../app.js';
 import request from 'supertest';
 
-describe('POST /weather', () => {
+describe('GET /movie', () => {
+  it('when full movie list requested, response is correct', async () => {
+    const response = await request(app).get('/movie').send();
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('json'),
+    );
+    expect(response.body.movies).toBeDefined();
+    expect(Array.isArray(response.body.movies)).toBe(true);
+  });
+  it('when a movie requested with an existing id, response is correct', async () => {
+    const id = 2;
+    const response = await request(app).get(`/movie/${id}`).send();
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('json'),
+    );
+    expect(response.body.id).toBeDefined();
+    expect(response.body.title).toBeDefined();
+    expect(response.body.director).toBeDefined();
+    expect(response.body.release_date).toBeDefined();
+  });
+  it('when a movie requested with wrong id, response contains error message', async () => {
+    const id = 100;
+    const response = await request(app).get(`/movie/${id}`).send();
+    expect(response.statusCode).toBe(404);
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('json'),
+    );
+    expect(response.body.serverMessage).toContain('ERROR');
+  });
+});
+
+describe('POST /movie', () => {
   it('when passed a correct request, a response contains required body in JSON and correct status code', async () => {
-    const city = 'London';
-    const response = await request(app).post('/weather').send({
-      cityName: city,
+    const title = 'New movie';
+    const director = 'New director';
+    const release_date = '2023-01-25';
+    const response = await request(app).post('/movie').send({
+      title: title,
+      director: director,
+      release_date: release_date,
     });
     expect(response.statusCode).toBe(200);
     expect(response.headers['content-type']).toEqual(
       expect.stringContaining('json'),
     );
-    expect(response.body.cityName).toEqual(city);
-    expect(response.body.weatherText).toBeDefined();
-    expect(response.body.weatherText).toContain(city);
+    expect(response.body.id).toBeDefined();
   });
-  it('when passed a request with no existing city name, a response contains error message and 404 status code', async () => {
-    const response = await request(app).post('/weather').send({
-      cityName: 'Londonn',
+  it('when passed a request with wrong fields, a response contains error message', async () => {
+    const id = '100';
+    const title = 'New movie';
+    const director = 'New director';
+    const release_data = '2023-01-25';
+    const response = await request(app).post('/movie').send({
+      id: id,
+      title: title,
+      director: director,
+      wrong_field: release_data,
     });
     expect(response.statusCode).toBe(400);
     expect(response.headers['content-type']).toEqual(
       expect.stringContaining('json'),
     );
-    expect(response.body.weatherText).toEqual('City is not found!');
+    expect(response.body.serverMessage).toContain('ERROR');
   });
-  it('when passed a correct request to a wrong endpoint', async () => {
-    const response = await request(app).post('/weaher/').send({
-      cityName: 'London',
-    });
-    expect(response.statusCode).toBe(404);
-    expect(response.body).toStrictEqual({});
+});
+
+describe('DELETE /movie', () => {
+  it('when passed a correct request, a response contains required body in JSON and correct status code', async () => {
+    const id = 2;
+    const response = await request(app).delete(`/movie/${id}`).send();
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('json'),
+    );
+    expect(response.body.serverMessage).toBeDefined();
+  });
+  it('when passed a request with wrong fields, a response contains error message', async () => {
+    const id = 100;
+    const response = await request(app).delete(`/movie/${id}`).send();
+    expect(response.statusCode).toBe(400);
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('json'),
+    );
+    expect(response.body.serverMessage).toContain('ERROR');
   });
 });
--- controllers/movies.js
+++ controllers/movies.js
@@ -1,14 +1,29 @@
+import { v4 } from 'uuid';
+
 import { data } from '../data.js';
 let movies = data;
 
 export const getAllMovies = (req, res) => {
-  res.status(200).json(movies);
+  res.status(200).json({ serverMessage: 'OK', movies: movies });
 };
 
 export const addMovie = (req, res) => {
-  const movie = req.body;
-  movies.push(movie);
-  res.status(200).send('Movie is added to the list');
+  const { title, director, release_date } = req.body;
+  if (title && director && release_date) {
+    const id = v4(); // unique id generator
+    movies.push({ id, title, director, release_date });
+    res.status(200).json({
+      serverMessage: 'Movie added',
+      id: id,
+    });
+  } else {
+    // Error message contains missed fields
+    res.status(400).json({
+      serverMessage: `ERROR. Please make sure your request contains JSON with fields:${
+        title ? '' : 'title '
+      } ${director ? '' : 'director '}${release_date ? '' : 'release_date'}`,
+    });
+  }
 };
 
 export const findMovie = (req, res) => {
@@ -20,20 +35,26 @@ export const findMovie = (req, res) => {
       return;
     }
   }
-
-  res.status(404).send('Movie is not found');
+  res.status(404).send({ serverMessage: 'ERROR 404: Movie is not found' });
 };
 
 export const deleteMovie = (req, res) => {
   const id = req.params.id;
-  const filteredMovies = movies.filter((movie) => {
-    if (movie.id !== id) {
-      return true;
-    } else {
-      return false;
-    }
+  const ids = movies.map((movie) => {
+    return movie.id;
   });
-  movies = filteredMovies;
 
-  res.status(200).send('Movie is deleted');
+  if (ids.includes(id)) {
+    const filteredMovies = movies.filter((movie) => {
+      if (movie.id !== id) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    movies = filteredMovies;
+    res.status(200).send({ serverMessage: 'Movie deleted' });
+  } else {
+    res.status(400).send({ serverMessage: 'ERROR: Movie is not found!' });
+  }
 };
